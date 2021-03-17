fab684bc30f971a0bd1c95cca2a40f4b3e510ad7
Konstantin-Tmf <timofeev.kntn@gmail.com>
2021-03-17 19:17:04 +0300

    final

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,41 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Мария", "Прокопенко", (byte) 32);
+        System.out.println("User Мария добавлена");
+
+        userService.saveUser("Юлия", "Васильева", (byte) 21);
+        System.out.println("User Юлия добавлена");
+
+        userService.saveUser("Владимир", "Сотников", (byte) 19);
+        System.out.println("User Владимир добавлен");
+
+        userService.saveUser("Александр", "Морозов", (byte) 39);
+        System.out.println("User Александр добавлен");
+
+        List<User> userList = userService.getAllUsers();
+        userList.forEach(System.out::println);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
     }
+
+
 }
+
